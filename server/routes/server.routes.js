c2c85fb3ea3263fbc0cbf97d7f2ecd8e1ce76bc5
Matt Sontag <mrsontag@gmail.com>
2020-11-17 20:52:37 -0500

    First draft of the server backend

---
+++ server/routes/server.routes.js
@@ -0,0 +1,16 @@
+const UserController = require("../controllers/user.controller");
+const PhotoController = require("../controllers/photo.controller");
+
+module.exports = app => {
+  app.get("/api/users/", UserController.findAllUsers);
+  app.get("/api/users/:id", UserController.findOneSingleUser);
+  app.put("/api/users/update/:id", UserController.updateExistingUser);
+  app.post("/api/users/new", UserController.createNewUser);
+  app.delete("/api/users/delete/:id", UserController.deleteAnExistingUser);
+
+  app.get("/api/photos/", PhotoController.findAllPhotos);
+  app.get("/api/photos/:id", PhotoController.findOneSinglePhoto);
+  app.put("/api/photos/update/:id", PhotoController.updateExistingPhoto);
+  app.post("/api/photos/new", PhotoController.createNewPhoto);
+  app.delete("/api/photos/delete/:id", PhotoController.deleteAnExistingPhoto);
+};
