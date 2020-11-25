e621209b9d7ab98840176dbeabd9e3ce8e2f8e63
Matt Sontag <mrsontag@gmail.com>
2020-11-25 18:29:46 -0500

    Created a bunch of basic interface components, mid-development

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider 
+        domain="dev-08umzn2o.us.auth0.com" 
+        clientId="76OKk6yoIt4f4W4ChvVmYytfgRcZqI1I"
+        redirectUri={ "http://localhost:3000/loggedin/"}
+        audience="https://localhost:8000/api"
+        scope="read:any">
+        <App />
+      </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
